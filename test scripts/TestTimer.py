62d9cdc1984a34675027ba125b87278f51ba090e
Stephen Landaas <77600263+stephenlandaas@users.noreply.github.com>
2022-03-07 00:57:34 -0800

    Update TestTimer.py

--- test scripts/TestTimer.py
+++ test scripts/TestTimer.py
@@ -9,14 +9,12 @@ def Test_timer():
     aqObject.CheckProperty(Aliases.browser.pageTuffytypes.FindElement("#restartButton"), "contentText", cmpEqual, "")
     #Selects the '15 Seconds' item of the 'selectTimer1' combo box.
     Aliases.browser.pageTuffytypes.selectTimer1.ClickItem("15 Seconds")
-    OCR.Recognize(Aliases.browser.wndChrome_WidgetWin_1.Chrome_RenderWidgetHostHWND).BlockByText("seconds", spLeftMost).Click()
     #Checks whether the 'contentText' property of the Aliases.browser.pageTuffytypes.FindElement("#timer") object equals 'Seconds Remaining: 15'.
     aqObject.CheckProperty(Aliases.browser.pageTuffytypes.FindElement("#timer"), "contentText", cmpEqual, "Seconds Remaining: 15")
     #Checks whether the 'contentText' property of the Aliases.browser.pageTuffytypes.FindElement("#restartButton") object equals ''.
     aqObject.CheckProperty(Aliases.browser.pageTuffytypes.FindElement("#restartButton"), "contentText", cmpEqual, "")
     #Selects the '30 Seconds' item of the 'selectTimer1' combo box.
     Aliases.browser.pageTuffytypes.selectTimer1.ClickItem("30 Seconds")
-    OCR.Recognize(Aliases.browser.wndChrome_WidgetWin_1.Chrome_RenderWidgetHostHWND).BlockByText("Seconds", spNearestToCenter).Click()
     #Checks whether the 'contentText' property of the Aliases.browser.pageTuffytypes.FindElement("#timer") object equals 'Seconds Remaining: 30'.
     aqObject.CheckProperty(Aliases.browser.pageTuffytypes.FindElement("#timer"), "contentText", cmpEqual, "Seconds Remaining: 30")
     #Checks whether the 'contentText' property of the Aliases.browser.pageTuffytypes.FindElement("#restartButton") object equals ''.
@@ -31,7 +29,6 @@ def Test_timer():
     aqObject.CheckProperty(Aliases.browser.pageTuffytypes.FindElement("#restartButton"), "contentText", cmpEqual, "")
     #Selects the '60 Seconds' item of the 'selectTimer1' combo box.
     Aliases.browser.pageTuffytypes.selectTimer1.ClickItem("60 Seconds")
-    OCR.Recognize(Aliases.browser.wndChrome_WidgetWin_1.Chrome_RenderWidgetHostHWND).BlockByText("seconds", spBottomMost).Click()
     #Checks whether the 'contentText' property of the Aliases.browser.pageTuffytypes.FindElement("#timer") object equals 'Seconds Remaining: 60'.
     aqObject.CheckProperty(Aliases.browser.pageTuffytypes.FindElement("#timer"), "contentText", cmpEqual, "Seconds Remaining: 60")
     #Checks whether the 'contentText' property of the Aliases.browser.pageTuffytypes.FindElement("#restartButton") object equals ''.
