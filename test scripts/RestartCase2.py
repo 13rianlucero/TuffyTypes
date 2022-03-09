90d64da0888524540c1edc98551f9b30a3fd658b
kchoum <48703108+kchoum@users.noreply.github.com>
2022-03-08 17:27:55 -0800

    Add files via upload

--- test scripts/RestartCase2.py
+++ test scripts/RestartCase2.py
@@ -3,13 +3,19 @@
     Delay(3000)
     Var1 = Aliases.browser.pageTuffytypes.FindElement("#generated-quote").contentText
     Log.Message(Var1)
-    aqObject.CheckProperty(Aliases.browser.pageTuffytypes.FindElement("#timer"), "contentText", cmpEqual, "Seconds Remaining: 60")
+    TimerVal1 = Aliases.browser.pageTuffytypes.FindElement("#timer").contentText
+    Log.Message(TimerVal1)
     Aliases.browser.pageTuffytypes.buttonRestartbutton.ClickButton()
     Delay(3000)
     Var2 = Aliases.browser.pageTuffytypes.FindElement("#generated-quote").contentText
     Log.Message(Var2)
+    TimerVal2 = Aliases.browser.pageTuffytypes.FindElement("#timer").contentText
+    Log.Message(TimerVal2)
     if Var1 != Var2:
       Log.Message("Restart button replaces the quote")
     else:
       Log.Message("Restart button does not replace the quote")
-    aqObject.CheckProperty(Aliases.browser.pageTuffytypes.FindElement("#timer"), "contentText", cmpEqual, "Seconds Remaining: 60")
+    if TimerVal1 == TimerVal2:
+      Log.Message("UI remains consistent after restart.")
+    else:
+      Log.Message("UI does not remain consistent after restart.")
